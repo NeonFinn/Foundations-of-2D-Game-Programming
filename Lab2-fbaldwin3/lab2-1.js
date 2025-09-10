aa6a0039f713764765dcbc2efa31289ea0b78929
NeonFinn <n3onfinn@gmail.com>
2025-09-10 00:21:44 -0400

    -

--- Lab2-fbaldwin3/lab2-1.js
+++ Lab2-fbaldwin3/lab2-1.js
@@ -6,29 +6,36 @@ var canvas = document.getElementById('snailbait-game-canvas'),
    runnerImage = new Image();
 
 function initializeImages() {
+   console.log("Initializing images");
+
    background.src = 'images/background.png';
    runnerImage.src = 'images/runner.png';
 
    background.onload = function (e) {
+      console.log("Background image loaded");
 
       startGame();
    };
 }
 
 function startGame() {
+   console.log("Starting game");
    draw();
 }
 
 function draw() {
+   console.log("Drawing frame");
    drawBackground();
    drawRunner();
 }
 
 function drawBackground() {
+   console.log("Drawing background");
    context.drawImage(background, 0, 0);
 }
 
 function drawRunner() {
+   console.log("Drawing runner");
    context.drawImage(runnerImage, 50, 280);
 }
 
